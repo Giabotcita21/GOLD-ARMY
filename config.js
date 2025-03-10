5c415637e4217580ecb818b341081059310e3e32
Giabotcita21 <157243253+Giabotcita21@users.noreply.github.com>
2025-03-10 18:32:23 -0500

    Update config.js

--- config.js
+++ config.js
@@ -16,57 +16,57 @@ global.isBaileysFail = false
 global.defaultLenguaje = 'es';
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
-  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215517489568', '💫 Mystic - Rey Endymion 💫', false],
-  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
-  ['573012482597', '💫 Mystic - Alex-X', true],
-  ['59175655964', '💫 Mystic - By Alba070503 💫', true],
-  ['5493794297363', '💫 Mystic - GabrielVz', true], 
-  ['393518398856', '💫 Mystic - Tester Fabri115💫', true],
-  ['212781160203', '💫 Mystic - Saad 💫', true], 
-  ['5217778573223', '💫 Mystic - Sarethh 💫', true],
-  ['595972157130'],
-  ['593968585283'],
-  ['5492266466080'],
-  ['5218442114446'],
-  ['59894808483'],
-  ['593980586516'], 
-  ['595975740803'],  
-  ['5492266613038'],
-  ['50497150165'],
-  ['51906662557'],
-  ['573183650526'], 
-  ['5217441298510'], 
-  ['5217294888993'],
-  ['595992611272'],
-  ['5219992843892'],
-  ['5493795319022'],
-  ['573147616444'], 
-  ['5219991402134'],
-  ['5219996125657'],
-  ['5492215034412'],
-  ['584125778026'],
-  ['5218444937407'],
-  ['5218713339906']
+  ['51990185642', '👑 GOLDARMY - Creador 👑', true],
+  ['51990185642', '💫 GOLDARMY - Collaborator 2 💫', true],
+  ['51990185642', '💫 GOLDARMY - Collaborator 3 💫', true],
+  ['51990185642', '💫 GOLDARMY - Collaborator 4 💫', true],
+  ['51990185642', '💫 GOLDARMY - Collaborator 5 💫', true],
+  ['51990185642', '💫 GOLDARMY - Collaborator 6 💫', true],
+  ['51990185642', '💫 GOLDARMY - Rey Endymion 💫', false],
+  ['51990185642', '💫 GOLDARMY - Collaborator 8 💫', true],
+  ['51990185642', '💫 GOLDARMY - Alex-X', true],
+  ['51990185642', '💫 GOLDARMY - By Alba070503 💫', true],
+  ['51990185642', '💫 GOLDARMY - GabrielVz', true], 
+  ['51990185642', '💫 GOLDARMY - Tester Fabri115💫', true],
+  ['51990185642', '💫 GOLDARMY - Saad 💫', true], 
+  ['51990185642', '💫 GOLDARMY - Sarethh 💫', true],
+  ['51990185642'],
+  ['51990185642'],
+  ['51990185642'],
+  ['51990185642'],
+  ['51990185642'],
+  ['51990185642'], 
+  ['51990185642'],  
+  ['51990185642'],
+  ['51990185642'],
+  ['51990185642'],
+  ['51990185642'], 
+  ['51990185642'], 
+  ['51990185642'],
+  ['51990185642'],
+  ['51990185642'],
+  ['51990185642'],
+  ['51990185642'], 
+  ['51990185642'],
+  ['51990185642'],
+  ['51990185642'],
+  ['51990185642'],
+  ['51990185642'],
+  ['51990185642']
 ];
 
-global.suittag = ['5219993404349'];
-global.prems = ['51995386439'];
+global.suittag = ['51990185642'];
+global.prems = ['51990185642'];
 
 // API
 global.BASE_API_DELIRIUS = "https://delirius-apiofc.vercel.app";
 
 global.packname = 'Sticker';
-global.author = 'The Mystic - Bot';
-global.wm = 'The Mystic - Bot';
-global.titulowm = 'The Mystic - Bot';
-global.titulowm2 = `The Mystic - Bot`
-global.igfg = 'The Mystic - Bot';
+global.author = '𝐆𝐨𝐥𝐝 - 𝐀𝐫𝐦𝐲';
+global.wm = '𝐆𝐨𝐥𝐝 - 𝐀𝐫𝐦𝐲';
+global.titulowm = '𝐆𝐨𝐥𝐝 - 𝐀𝐫𝐦𝐲';
+global.titulowm2 = `𝐆𝐨𝐥𝐝 - 𝐀𝐫𝐦𝐲`
+global.igfg = '𝐆𝐨𝐥𝐝 - 𝐀𝐫𝐦𝐲';
 global.wait = '*_[ ⏳ ] Cargando..._*';
 
 /*global.imagen1 = fs.createReadStream('./src/assets/images/menu/languages/es/menu.png');
@@ -101,7 +101,7 @@ global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
 global.waitt = '*_[ ⏳ ] Cargando..._*';
 global.waittt = '*_[ ⏳ ] Cargando..._*';
 global.waitttt = '*_[ ⏳ ] Cargando..._*';
-global.nomorown = '5219993404349';
+global.nomorown = '51990185642';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
